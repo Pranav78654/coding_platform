660a50bdc8a446911d3b5259d70fad368a4172b4
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-08-19 13:54:33 +0530

    connected mongodb

--- config/database.js
+++ config/database.js
@@ -15,3 +15,18 @@
 // };
 
 // module.exports = connectDB;
+const mongoose = require("mongoose");
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully');
+
+    }catch(error){
+        console.error("mongodb connection failed",error.message)
+
+
+    }
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-const express = require('express');
-const cookieParser = require("cookie-parser");
+// const express = require('express');
+// const cookieParser = require("cookie-parser");
 
-require('dotenv').config();
-const connectDB = require('./config/database');
+// require('dotenv').config();
+// const connectDB = require('./config/database');
 
-const userRoutes = require('./routes/userRoute');
-const errorHandler = require('./middleware/errorHandler');
-const profileRouter = require('./routes/profile');
-const sessionRoutes = require('./routes/sessionRoute');
-const handRaiseRoute = require('./routes/handRaiseRoute');
+// const userRoutes = require('./routes/userRoute');
+// const errorHandler = require('./middleware/errorHandler');
+// const profileRouter = require('./routes/profile');
+// const sessionRoutes = require('./routes/sessionRoute');
+// const handRaiseRoute = require('./routes/handRaiseRoute');
+// const app = express();
+
+// // Middleware
+// app.use(cookieParser());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/auth', userRoutes);
+// app.use('/api/',profileRouter);
+// app.use('/api/session', sessionRoutes);
+// app.use('/api/hand',handRaiseRoute);
+// // Error Handling Middleware
+// app.use(errorHandler);
+
+// // Start server after DB connects
+// const PORT = process.env.PORT || 5000;
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`);
+//     });
+// });
+
+const express = require("express");
+const connectDB = require("./config/database");
+require("dotenv").config();
+
 const app = express();
 
-// Middleware
-app.use(cookieParser());
-app.use(express.json());
+connectDB();
 
-// Routes
-app.use('/api/auth', userRoutes);
-app.use('/api/',profileRouter);
-app.use('/api/session', sessionRoutes);
-app.use('/api/hand',handRaiseRoute);
-// Error Handling Middleware
-app.use(errorHandler);
+app.get("/",(req,res)=>{
+    res.send("api is working");
 
-// Start server after DB connects
-const PORT = process.env.PORT || 5000;
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-});
+})
+
+const PORT = process.env.PORT || 3007;
+
+app.listen(PORT,()=> console.log(`Server is running on ${PORT}`));
+
+
